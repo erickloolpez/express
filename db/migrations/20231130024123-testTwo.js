db3bf4341738640d2282fd6429985416046a6444
erickloolpez <erickloopeze@hotmai.com>
2023-11-29 22:20:45 -0500

    First changes no newBranch8

--- db/migrations/20231130024123-testTwo.js
+++ db/migrations/20231130024123-testTwo.js
@@ -5,12 +5,12 @@ const {usuarioSchema, USUARIO_TABLE} = require('../models/usario.model')
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable(CARRITO_TABLE, carritoSchema)
     await queryInterface.createTable(USUARIO_TABLE, usuarioSchema)
+    await queryInterface.createTable(CARRITO_TABLE, carritoSchema)
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable(CARRITO_TABLE)
     await queryInterface.dropTable(USUARIO_TABLE)
+    await queryInterface.dropTable(CARRITO_TABLE)
   }
 };
